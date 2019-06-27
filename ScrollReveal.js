ab1f17e80930f9878720ddab11a05e6ad1be6283
Eamonn Craig <32011263+ecraig12@users.noreply.github.com>
2019-06-27 17:29:14 +0100

    Add files via upload

---
+++ ScrollReveal.js
@@ -0,0 +1,30 @@
+//Place the below in the head tag:
+
+<script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>  
+
+//In the code below, the #about, #services, #projects & #contact are the section IDs of my website. 
+//Duration is in milliseconds, origin & distance are self explanatory. 
+
+<script>
+window.sr = ScrollReveal();
+sr.reveal('#about div', {
+duration: 1500,
+origin: 'left',
+distance:'300px'
+});
+sr.reveal('#services div', {
+duration: 1500,
+origin: 'right',
+distance:'300px'
+});
+sr.reveal('#projects div', {
+duration: 1500,
+origin: 'left',
+distance: '300px'
+});
+sr.reveal('#contact div', {
+duration: 1500,
+origin: 'bottom',
+distance: '600px'
+});
+</script>
