f7e915d38a4c47c9fa1729f7bf26d0eb6176fc5d
Eamonn Craig <32011263+ecraig12@users.noreply.github.com>
2019-06-28 12:00:44 +0100

    Update ScrollReveal.js

--- ScrollReveal.js
+++ ScrollReveal.js
@@ -2,29 +2,29 @@
 
 <script src="https://unpkg.com/scrollreveal/dist/scrollreveal.min.js"></script>  
 
-//In the code below, the #about, #services, #projects & #contact are the section IDs of my website. 
-//Duration is in milliseconds, origin determines which direction element appears from, & distance is distance element will be revealed from. 
+//In the code below, the #about, #services, #projects & #contact are the section IDs of an example website. 
+//Duration is in milliseconds, origin determines which direction element appears from, & distance is the distance element will be revealed from. 
 
 <script>
-window.sr = ScrollReveal();
-sr.reveal('#about div', {
-duration: 1500,
-origin: 'left',
-distance:'300px'
-});
-sr.reveal('#services div', {
-duration: 1500,
-origin: 'right',
-distance:'300px'
-});
-sr.reveal('#projects div', {
-duration: 1500,
-origin: 'left',
-distance: '300px'
-});
-sr.reveal('#contact div', {
-duration: 1500,
-origin: 'bottom',
-distance: '600px'
-});
+      window.sr = ScrollReveal();
+      sr.reveal('#about div', {
+      duration: 1500,
+      origin: 'left',
+      distance:'300px'
+      });
+      sr.reveal('#services div', {
+      duration: 1500,
+      origin: 'right',
+      distance:'300px'
+      });
+      sr.reveal('#projects div', {
+      duration: 1500,
+      origin: 'left',
+      distance: '300px'
+      });
+      sr.reveal('#contact div', {
+      duration: 1500,
+      origin: 'bottom',
+      distance: '600px'
+      });
 </script>
